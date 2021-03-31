96f5d6c298ef269fdbb5932385067e9831982033
Cristinaro97 <cristinagrl@hotmail.com>
2021-03-31 11:31:44 -0500

    Avances diagrama API

--- index.js
+++ index.js
@@ -1,3 +1,83 @@
-module.exports = () => {
-  // ...
+const path = require('path');
+
+const fs = require('fs');
+
+const rute1 = '/home/laboratoria/Documents/LIM014-mdlinks/mdtest/readme.md';
+// almacenando la ruta
+
+// const markdown = require('markdown-js');
+
+// const marked = require('marked');
+
+// const jsdom = require('jsdom');
+
+function validatePath(rute) {
+  return path.isAbsolute(rute) === true ? rute : path.resolve(rute);
+  // operador ternario condiciones ? expre1 :expre2
+  // Validando la ruta
+}
+const rutaAbsoluta = validatePath(rute1);
+// convirtiendo y guardando la ruta en absoluta
+console.log(rutaAbsoluta);
+
+function validateIfPathExists(rute) { // preguntar si la ruta existe
+  return fs.existsSync(rute);
+  // estoy jalando el módulo fs, fs se encarga de ver el archivo, si es file o escritorio
+}
+// file? verfificar si es directorio
+const isItFile = (rute) => {
+  const file = fs.statSync(rute);
+  const resultFile = file.isDirectory(rute);
+  return resultFile;
+};
+console.log(isItFile('/home/laboratoria/Documents/LIM014-mdlinks/mdtest/hola'));
+
+/* const isItDirectory = fs.statSync(rute);
+console.log('is directory ? ' + isItDirectory.isDirectory()); */
+
+// Identificar si la  ruta es File o Directorio
+const isDir = (rute) => {
+  const stats = fs.statSync(rute);
+  const isDirectory = stats.isDirectory(rute);
+  return isDirectory;
+};
+console.log(isDir);
+
+// Identificar la extensión del archivo
+const extMD = (rute) => path.extname(rute);
+console.log(extMD(rute1));
+
+const readDir = (rute) => {
+  let allMD = [];
+  const dataDir = fs.readdirSync(rute);
+  console.log(dataDir);
+  dataDir.forEach((files) => {
+    const filePath = path.join(rute, files);
+    if (extMD(filePath) === '.md') {
+      allMD.push(filePath);
+    } else if (isDir(filePath) === true) {
+      allMD = allMD.concat(readDir(filePath));
+    }
+  });
+  return allMD;
+};
+console.log(readDir('/home/laboratoria/Documents/LIM014-mdlinks/mdtest/hola'));
+
+// Leyendo el archivo
+const readArchive = (rute) => fs.readFileSync(rute, 'utf-8');
+console.log(readArchive(rute1));
+
+// Contiene links?
+const existsLink = fs.existsSync(rute1);
+console.log(existsLink);
+
+/* const matchs = /\[(.+)\]\((https?:\/\/[^\s]+)(?: "(.+)")?\)|(https?:\/\/[^\s]+)/ig.exec(rute1);
+console.log(matchs); */
+
+module.exports = {
+  validatePath,
+  validateIfPathExists,
+  isDir,
+  readDir,
+  readArchive,
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,44 @@
-const mdLinks = require('../');
+const {
+  validatePath,
+  validateIfPathExists,
+  isDir,
+  readDir,
+  readArchive,
+} = require('../index.js');
 
-
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe('ValidatePath is a function?', () => {
+  it('should be a function', () => {
+    expect(typeof validatePath).toBe('function');
   });
+});
+describe('Validateifpathexists is a function?', () => {
+  it('should be a function', () => {
+    expect(typeof validateIfPathExists).toBe('function');
+  });
+});
 
+describe('isDir is a function', () => {
+  it('should be a function', () => {
+    expect(typeof isDir).toBe('function');
+  });
+  it('return one value', () => {
+    expect(isDir('/home/laboratoria/Documents/LIM014-mdlinks/mdtest/hola')).toEqual(true);
+  });
+});
+
+describe('readDir is a function', () => {
+  it('Should be a function', () => {
+    expect(typeof readDir).toBe('function');
+  });
+  it('return one value', () => {
+    expect(readDir('/home/laboratoria/Documents/LIM014-mdlinks/mdtest/hola')).toEqual(['/home/laboratoria/Documents/LIM014-mdlinks/mdtest/hola/hola.md', '/home/laboratoria/Documents/LIM014-mdlinks/mdtest/hola/hola2/hola2.md']);
+  });
+});
+describe('readArchive is a function?', () => {
+  it('should be a function', () => {
+    expect(typeof readArchive).toBe('function');
+  });
+});
+it('return one value', () => {
+  expect(readArchive('/home/laboratoria/Documents/LIM014-mdlinks/mdtest/hola/hola.md')).toEqual('Hola mundo');
 });
